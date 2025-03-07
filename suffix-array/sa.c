c3f232af9e4b572b002428d0dd4ac0716830800d
Eduardo Díaz <ediaz@dcc8090.cl>
2025-03-07 18:23:12 -0300

    add search longest duplicate

--- suffix-array/sa.c
+++ suffix-array/sa.c
@@ -30,7 +30,7 @@ int* build_suffix_array(char *txt, int n) {
   return suffix_arr;
 }
 
-int search(char* pattern, char* txt, int *suffix_arr, int n) {
+int search(char* pattern, char* txt, int suffix_arr[], int n) {
   int m = strlen(pattern);
   int l = 0, r = n-1;
   while (l <= r) {
@@ -43,6 +43,42 @@ int search(char* pattern, char* txt, int *suffix_arr, int n) {
     else l = mid + 1;
   }
   return -1;
+}
+
+void search_longest_duplicate(char* txt, int* sa, int* lcp, int m, int n) {
+  int maxlen = lcp[0];
+  for (int i = 1; i < n; i++) {
+    if (lcp[i] > maxlen) maxlen = lcp[i];
+  }
+  printf("longest duplicate string:\n");
+  for (int i = 1; i < n; i++) {
+    if (lcp[i] == maxlen) {
+      int j = sa[i-1];
+      int h = lcp[i];
+      printf("%.*s\n", h, txt+j); 
+    }
+  }
+}
+
+int* kasai(char *txt, int sa[], int n) {
+  int* rank = (int*) malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++) {
+    rank[sa[i]] = i;
+  }
+  int *lcp = (int*) malloc(sizeof(int) * n);
+  int h = 0; 
+  for (int i = 0; i < n; i++) {
+    if (rank[i] > 0) {
+      int k = sa[rank[i]-1];
+      while (txt[i+h] == txt[k+h]) {
+        h++;
+      }
+      lcp[rank[i]] = h;
+      if (h > 0) h--;
+    }
+  }
+  free(rank);
+  return lcp;
 }
 
 char* read_text(const char* filename) {
@@ -61,6 +97,14 @@ char* read_text(const char* filename) {
   return buf;
 }
 
+void dump_array(const char* name, int* arr, int n) {
+  printf("%s: ", name);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
 int main(int argc, char* argv[]) {
 
   if (argc != 3) {
@@ -77,8 +121,12 @@ int main(int argc, char* argv[]) {
 
   char* pattern = argv[1];
 
+
   int n = strlen(txt);
+  int m = strlen(pattern);
   int* sa = build_suffix_array(txt, n);
+  int* lcp = kasai(txt, sa, n);
   printf("pattern found at position: %d\n",search(pattern, txt, sa, n));
+  search_longest_duplicate(txt, sa, lcp, m, n);
   return 0;
 }
